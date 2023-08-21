534c02233c5a1f620f148a557486bf27212b3aea
Roy Pereira <roymap67@gmail.com>
2023-08-21 19:48:52 -0400

    added DC

--- README.md
+++ README.md
@@ -1,27 +1,58 @@
-# UnifiedAngularIntegrationsDirectory
+<h1>
+    <a href="https://unified.to"><img src="https://unified.to/images/logo.png" /></a>
+</h1>
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.1.0.
+# Unified.to's Integrations Directory Typescript Angular Component
 
-## Development server
+## Installing
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
+### Package manager
 
-## Code scaffolding
+Using NPM:
 
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
+```bash
+$ npm install unified-angular-integrations-directory
+```
 
-## Build
+Using yarn:
 
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory.
+```bash
+$ yarn add unified-angular-integrations-directory
+```
 
-## Running unit tests
+Once the package is installed, you can import the component using `import`
 
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
+```ts
+import UnifiedDirectory from 'unified-angular-integrations-directory';
+```
 
-## Running end-to-end tests
+## Example
 
-Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To use this command, you need to first add a package that implements end-to-end testing capabilities.
+```js
+return (
+    <>
+        <h1>Integrations Directory</h1>
+        <UnifiedDirectory workspaceId="12345" />
+    </>
+);
+```
 
-## Further help
+The following are the `props` that can be passed to the component:
 
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+```ts
+{
+    workspace_id: string;    // your workspace_id found at https://app.unified.to/settings/api
+    categories?: string[];  // An array of integration categories to limit the list of integrations (crm, ats, hr, uc,. ticketing, martech)
+    external_xref?: string; // Your ID for the account/user that is signed into your application
+    state?: string;         // A state string that will be sent back to your success URL
+    scopes?: string[];      // An array of Unified.to permission scopes to request from OAUTH2-based integrations found at https://unified.to/apidocs#unified_object_connection
+    success_redirect?: string;  // The URL where you want the user to be redirect to after a successful authentication. The Integration ID will be appended with (id=) to this URL, as will the state provided
+    failure_redirect?: string;  // The URL where you want the user to be redirect to after an unsuccessful or aborted authorization.  An 'error' variable will be appended.
+    nostyle?: boolean;
+    environment?: string;   // The development environment (Eg. Production, Sandbox, ...)
+    lang?: string;          // The language for the API Key authorization page.  (en, fr, es, it, pt, hi, zh)
+    notabs?: boolean;       // Do not display tabs in the embedded directory
+    nocategories?: boolean; // Do not display category badges for each integration
+    dc?: 'us'|'eu';         // data-region
+}
+```
--- projects/unified-angular-directory/src/lib/unified-angular-directory.component.ts
+++ projects/unified-angular-directory/src/lib/unified-angular-directory.component.ts
@@ -1,213 +1,175 @@
-import {
-  Component,
-  Input,
-  OnInit,
-  OnChanges,
-  SimpleChanges,
-} from '@angular/core';
+import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 
 import { IIntegration, TIntegrationCategory } from './models/Unified';
 import { CommonModule } from '@angular/common';
 
-const APIURL = 'https://api.unified.to';
-type TIntegrationCategoryType = Exclude<TIntegrationCategory, 'auth' |'passthrough'>;
+const API_NA_URL = 'https://api.unified.to';
+const API_EU_URL = 'https://api-eu.unified.to';
+
+type TIntegrationCategoryType = Exclude<TIntegrationCategory, 'auth' | 'passthrough'>;
 
 @Component({
-  imports: [CommonModule],
-  standalone: true,
-  selector: 'unified',
-  template: `
-    <div class="unified">
-      <div
-        class="unified_menu"
-        *ngIf="!notabs && CATEGORIES.length > 0 && filter(INTEGRATIONS).length"
-      >
-        <button
-          class="unified_button unified_button_all"
-          [ngClass]="{selectedCategory ? '' : 'active'}"
-          (click)="unified_select_category()"
-        >
-          All
-        </button>
-        <button
-          *ngFor="let cat of CATEGORIES"
-          class="unified_button unified_button_"
-          [ngClass]="[cat,{selectedCategory === cat ? 'active' : ''}]"
-          (click)="unified_select_category(cat)"
-        >
-          {{ CATEGORY_MAP[cat as TIntegrationCategory] }}
-        </button>
-      </div>
-      <div class="unified_vendors">
-        <a
-          *ngFor="let integration of filter(INTEGRATIONS)"
-          href="{unified_get_auth_url(integration)}"
-          class="unified_vendor"
-        >
-          <img src="{integration.logo_url}" class="unified_image" />
-          <div class="unified_vendor_inner">
-            <div class="unified_vendor_name">{{ integration.name }}</div>
-            <div
-              *ngIf="!nocategories"
-              class="unified_vendor_cats"
-              *ngFor="let cat of integration.categories.filter((c) => !CATEGORIES || CATEGORIES.indexOf(c) > -1)"
-            >
-              <span>{{ CATEGORY_MAP[cat] }}</span>
+    imports: [CommonModule],
+    standalone: true,
+    selector: 'unified',
+    template: `
+        <div class="unified">
+            <div class="unified_menu" *ngIf="!notabs && CATEGORIES.length > 0 && filter(INTEGRATIONS).length">
+                <button class="unified_button unified_button_all" [ngClass]="{selectedCategory ? '' : 'active'}" (click)="unified_select_category()">
+                    All
+                </button>
+                <button
+                    *ngFor="let cat of CATEGORIES"
+                    class="unified_button unified_button_"
+                    [ngClass]="[cat,{selectedCategory === cat ? 'active' : ''}]"
+                    (click)="unified_select_category(cat)"
+                >
+                    {{ CATEGORY_MAP[cat as TIntegrationCategory] }}
+                </button>
             </div>
-          </div>
-        </a>
-        <div *ngIf="!filter(INTEGRATIONS).length">
-          No integrations available
+            <div class="unified_vendors">
+                <a *ngFor="let integration of filter(INTEGRATIONS)" href="{unified_get_auth_url(integration)}" class="unified_vendor">
+                    <img src="{integration.logo_url}" class="unified_image" />
+                    <div class="unified_vendor_inner">
+                        <div class="unified_vendor_name">{{ integration.name }}</div>
+                        <div
+                            *ngIf="!nocategories"
+                            class="unified_vendor_cats"
+                            *ngFor="let cat of integration.categories.filter((c) => !CATEGORIES || CATEGORIES.indexOf(c) > -1)"
+                        >
+                            <span>{{ CATEGORY_MAP[cat] }}</span>
+                        </div>
+                    </div>
+                </a>
+                <div *ngIf="!filter(INTEGRATIONS).length">No integrations available</div>
+            </div>
         </div>
-      </div>
-    </div>
-  `,
+    `,
 })
 export class UnifiedAngularDirectoryComponent implements OnInit, OnChanges {
-  @Input() INTEGRATIONS: IIntegration[] = [];
-  @Input() selectedCategory?: TIntegrationCategory;
-  @Input() CATEGORIES: TIntegrationCategory[] = [];
-  @Input() CATEGORY_MAP: { [p in TIntegrationCategoryType]: string } = {
-    crm: 'CRM',
-    martech: 'Marketing',
-    ticketing: 'Ticketing',
-    // auth: 'Authentication',
-    uc: 'Unified Communications',
-    enrich: 'Enrichment',
-    ats: 'ATS',
-    hris: 'HR',
-    // passthrough: 'Passthrough',
-  };
+    @Input() INTEGRATIONS: IIntegration[] = [];
+    @Input() selectedCategory?: TIntegrationCategory;
+    @Input() CATEGORIES: TIntegrationCategory[] = [];
+    @Input() CATEGORY_MAP: { [p in TIntegrationCategoryType]: string } = {
+        crm: 'CRM',
+        martech: 'Marketing',
+        ticketing: 'Ticketing',
+        uc: 'Unified Communications',
+        enrich: 'Enrichment',
+        ats: 'ATS',
+        hris: 'HR',
+    };
+    @Input() workspace_id?: { type: string; required: true };
+    @Input() categories?: string[];
+    @Input() external_xref?: string;
+    @Input() state?: string;
+    @Input() scopes?: string[];
+    @Input() success_redirect?: string;
+    @Input() failure_redirect?: string;
+    @Input() nostyle?: boolean;
+    @Input() environment?: string;
+    @Input() lang?: string;
+    @Input() nocategories?: boolean;
+    @Input() notabs?: boolean;
+    @Input() dc?: 'us' | 'eu'; // which data-center
 
-  @Input() workspace_id?: { type: string; required: true };
-  @Input() categories?: string[];
-  @Input() external_xref?: string;
-  @Input() state?: string;
-  @Input() scopes?: string[];
-  @Input() success_redirect?: string;
-  @Input() failure_redirect?: string;
-  @Input() nostyle?: boolean;
-  @Input() environment?: string;
-  @Input() lang?: string;
-  @Input() nocategories?: boolean;
-  @Input() notabs?: boolean;
-
-  async ngOnInit() {
-    await this.setup();
-  }
-
-  async ngOnChanges(changes: SimpleChanges) {
-    if (
-      changes['workspaceId'].currentValue !==
-        changes['workspaceId'].previousValue ||
-      changes['categories'].currentValue !==
-        changes['categories'].previousValue ||
-      changes['environment'].currentValue !==
-        changes['environment'].previousValue
-    ) {
-      await this.setup();
+    async ngOnInit() {
+        await this.setup();
     }
-  }
 
-  filter(integrations: IIntegration[]) {
-    return (
-      integrations?.filter(
-        (integration) =>
-          !this.selectedCategory ||
-          integration.categories.includes(this.selectedCategory)
-      ) || []
-    );
-  }
-
-  unified_get_auth_url(integration: IIntegration) {
-    let url = `${APIURL}/unified/integration/auth/${this.workspace_id}/${integration.type}?redirect=1`;
-
-    if (this.external_xref) {
-      url += `&user_xref=${encodeURIComponent(this.external_xref)}`;
-    }
-    if (this.state) {
-      url += `&state=${encodeURIComponent(this.state)}`;
-    }
-    if (this.scopes?.length) {
-      url += `&scopes=${encodeURIComponent(this.scopes.join(','))}`;
-    }
-    if (this.lang) {
-      url += `&lang=${this.lang}`;
-    }
-    if (this.environment && this.environment !== 'Production') {
-      url += `&env=${encodeURIComponent(this.environment)}`;
-    }
-    // if (this.success_redirect) {
-    url += `&success_redirect=${encodeURIComponent(
-      this.success_redirect || window.location.href
-    )}`;
-    // }
-    // if (this.failure_redirect) {
-    url += `&failure_redirect=${encodeURIComponent(
-      this.failure_redirect || window.location.href
-    )}`;
-    // }
-    return url;
-  }
-
-  unified_select_category(category?: TIntegrationCategory) {
-    this.selectedCategory = category;
-  }
-
-  async load_data<T>(url: string) {
-    try {
-      const response = await fetch(url, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-      return response.json() as T;
-    } catch {
-      return;
-    }
-  }
-
-  async setup() {
-    this.selectedCategory = undefined;
-    const url = `${APIURL}/unified/integration/workspace/${
-      this.workspace_id
-    }?summary=1${
-      this.categories?.length ? '&categories=' + this.categories.join(',') : ''
-    }${
-      this.environment === 'Production' || !this.environment
-        ? ''
-        : '&env=' + encodeURIComponent(this.environment)
-    }`;
-
-    this.INTEGRATIONS = (await this.load_data<IIntegration[]>(url)) || [];
-
-    this.CATEGORIES = [];
-    this.INTEGRATIONS.forEach((integration) => {
-      integration.categories?.forEach((c) => {
+    async ngOnChanges(changes: SimpleChanges) {
         if (
-          this.CATEGORY_MAP[c as TIntegrationCategoryType
-          ] &&
-          (!this.categories?.length || this.categories.includes(c))
+            changes['workspaceId'].currentValue !== changes['workspaceId'].previousValue ||
+            changes['categories'].currentValue !== changes['categories'].previousValue ||
+            changes['environment'].currentValue !== changes['environment'].previousValue
         ) {
-          this.CATEGORIES.push(c);
+            await this.setup();
         }
-      });
-    });
-    this.CATEGORIES = [...new Set(this.CATEGORIES)];
-    if (this.CATEGORIES.length === 1) {
-      this.CATEGORIES = [];
-    } else {
-      this.CATEGORIES = this.CATEGORIES.sort(function (a, b) {
-        return a.localeCompare(b);
-      });
     }
 
-    if (!this.nostyle) {
-      const link = document.createElement('link');
-      link.href = `${APIURL}/docs/unified.css`;
-      link.rel = 'stylesheet';
-      document.head.appendChild(link);
+    filter(integrations: IIntegration[]) {
+        return integrations?.filter((integration) => !this.selectedCategory || integration.categories.includes(this.selectedCategory)) || [];
     }
-  }
+
+    unified_get_auth_url(integration: IIntegration) {
+        const APIURL = this.dc === 'eu' ? API_EU_URL : API_NA_URL;
+
+        let url = `${APIURL}/unified/integration/auth/${this.workspace_id}/${integration.type}?redirect=1`;
+
+        if (this.external_xref) {
+            url += `&user_xref=${encodeURIComponent(this.external_xref)}`;
+        }
+        if (this.state) {
+            url += `&state=${encodeURIComponent(this.state)}`;
+        }
+        if (this.scopes?.length) {
+            url += `&scopes=${encodeURIComponent(this.scopes.join(','))}`;
+        }
+        if (this.lang) {
+            url += `&lang=${this.lang}`;
+        }
+        if (this.environment && this.environment !== 'Production') {
+            url += `&env=${encodeURIComponent(this.environment)}`;
+        }
+        // if (this.success_redirect) {
+        url += `&success_redirect=${encodeURIComponent(this.success_redirect || window.location.href)}`;
+        // }
+        // if (this.failure_redirect) {
+        url += `&failure_redirect=${encodeURIComponent(this.failure_redirect || window.location.href)}`;
+        // }
+        return url;
+    }
+
+    unified_select_category(category?: TIntegrationCategory) {
+        this.selectedCategory = category;
+    }
+
+    async load_data<T>(url: string) {
+        try {
+            const response = await fetch(url, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+            return response.json() as T;
+        } catch {
+            return;
+        }
+    }
+
+    async setup() {
+        const APIURL = this.dc === 'eu' ? API_EU_URL : API_NA_URL;
+
+        this.selectedCategory = undefined;
+        const url = `${APIURL}/unified/integration/workspace/${this.workspace_id}?summary=1${
+            this.categories?.length ? '&categories=' + this.categories.join(',') : ''
+        }${this.environment === 'Production' || !this.environment ? '' : '&env=' + encodeURIComponent(this.environment)}`;
+
+        this.INTEGRATIONS = (await this.load_data<IIntegration[]>(url)) || [];
+
+        this.CATEGORIES = [];
+        this.INTEGRATIONS.forEach((integration) => {
+            integration.categories?.forEach((c) => {
+                if (this.CATEGORY_MAP[c as TIntegrationCategoryType] && (!this.categories?.length || this.categories.includes(c))) {
+                    this.CATEGORIES.push(c);
+                }
+            });
+        });
+        this.CATEGORIES = [...new Set(this.CATEGORIES)];
+        if (this.CATEGORIES.length === 1) {
+            this.CATEGORIES = [];
+        } else {
+            this.CATEGORIES = this.CATEGORIES.sort(function (a, b) {
+                return a.localeCompare(b);
+            });
+        }
+
+        if (!this.nostyle) {
+            const link = document.createElement('link');
+            link.href = `${APIURL}/docs/unified.css`;
+            link.rel = 'stylesheet';
+            document.head.appendChild(link);
+        }
+    }
 }
